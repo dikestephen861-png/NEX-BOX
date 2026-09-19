50036be55f786e65816bc06bec4f0a3de6814272
dikestephen861-png <dikestephen861@gmail.com>
2026-09-19 10:21:38 +0100

    Update service worker cache and file management

--- sw.js
+++ sw.js
@@ -1,23 +1,59 @@
-const CACHE_NAME = "nex-box-v1";
+const CACHE_NAME = "nex-box-v3";
 
-const FILES_TO_CACHE = [
+const APP_FILES = [
   "./",
   "./index.html",
+  "./watch.html",
+  "./library.html",
+  "./style.css",
+  "./app.js",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
+      return cache.addAll(APP_FILES);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
   );
+});
+
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+
 });
